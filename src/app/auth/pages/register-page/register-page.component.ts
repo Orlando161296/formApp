aeab1515eb490faee1da31f6b25fcc64721aed77
Orlando Rojas <josuerojas161296@gmail.com>
2023-10-08 12:41:23 -0400

    Fin de la sección 18 validaciones dentro de Formularios reactivos.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import * as customValidators from 'src/app/shared/validators/validators';
+import { ValidatorService } from '../../../shared/service/validators.service';
+import { EmailValidator } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
 
+    public myForm: FormGroup = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern( this.validatorService.firstnameAndLastnamePattern )]],
+      // email:['', [Validators.required, Validators.pattern(this.validatorService.emailPattern) ], [ new EmailValidator() ] ],
+      email:['', [Validators.required, Validators.pattern(this.validatorService.emailPattern) ], [ this.emailValidator ] ],
+      username: ['', [Validators.required, this.validatorService.cantBeStrider ]],
+      password: ['',[ Validators.required, Validators.minLength(6)]],
+      password2: ['',[ Validators.required]],
+    }, {
+      validators: [
+          this.validatorService.isFieldOneEqualFieldTwo('password','password2')
+      ]
+    });
+
+    constructor(
+      private fb: FormBuilder,
+      private validatorService: ValidatorService,
+      private emailValidator: EmailValidator,
+      ){}
+
+    isValidField( field: string){
+      //TODO: obtener validación desde un servicio.
+      return this.validatorService.isValidField( this.myForm, field )
+    }
+
+    onSubmit(){
+      this.myForm.markAllAsTouched();
+    }
 }
